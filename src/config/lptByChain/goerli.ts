59ac128e0ad7fd78207448af17fc14d79731966e
0xwrongchain <wrongchain@unbound.finance>
2022-12-06 14:34:35 +0530

    chore: update vault array

--- src/config/lptByChain/goerli.ts
+++ src/config/lptByChain/goerli.ts
@@ -4,6 +4,20 @@ export default {
     exchange: "Uniswap",
     token0: "ETH",
     token1: "DAI",
-    borrowOperations: "0xf98a8Fe868C7F963bA6B5a4523B2Bc932801d49c", // Used for getting collateral price
+    borrowOperations: "0x19dc6BDC4152F20076a48398748B4C07Fe4fCaAA", // Used for getting collateral price
+  },
+  "0x7E8e95CAcc86CB932fB54fA819DbC0d7A4535c09": {
+    name: "DAI-USDC",
+    exchange: "Uniswap",
+    token0: "DAI",
+    token1: "USDC",
+    borrowOperations: "0x1707713C03DDF449D7fF8A98C4EfAea72A71d372", // Used for getting collateral price
+  },
+  "0x8da3a0537465348FAFfb3a0D66Bc3D5ced90Ebaa": {
+    name: "ETH-WBTC",
+    exchange: "Uniswap",
+    token0: "ETH",
+    token1: "WBTC",
+    borrowOperations: "0xfe1d157871dd9F5CDa92C62E26CA2DB8Eb439a71", // Used for getting collateral price
   },
 };
